98518148f1881ae5e798a33247c08169b2e1a11e
Apple Artic <techapple652@gmail.com>
2022-11-24 17:26:17 +0530

    added some JS functionality

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let current="";
-function myFunc(e){
-    const btnPressed=e.target.className;
-    if(btnPressed==="number"){
-        current+=e.target.innerText;
-    }else if(e.target.id==="dot"){}
-    document.getElementById('current').textContent=`${current}`;
+let current = "";
+let dotIsPressed = false;
+
+function myFunc(e) {
+    const btnPressed = e.target;
+    if (btnPressed.className === "number") {
+        current += e.target.innerText;
+    } else if (btnPressed.className === "dot" && dotIsPressed===false) {
+        dotIsPressed = true;
+        current += e.target.innerText;
+    }
+    else if(btnPressed.className==="operator"){
+    //    TODO: when operator is pressed...
+    }
+    document.getElementById('current').textContent = `${current}`;
 }
+
 window.addEventListener('click', myFunc);
