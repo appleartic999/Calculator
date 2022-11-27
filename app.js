b9927833f42a9a05f2de7e58ee43cb643ba6c7c0
Apple Artic <techapple652@gmail.com>
2022-11-27 11:41:51 +0530

    completed Calculator

--- app.js
+++ app.js
@@ -1,18 +1,77 @@
-let current = "";
+let current = "", currentValue = 0, operator = "", result = 0;
 let dotIsPressed = false;
+let arr = [];//acts as a QUEUE data structure to evaluate values
+function evaluate() {
+    if (arr.length === 1) {
+        currentValue = arr[0];
+    } else if (arr.length === 3) {
+        if (arr[1] === "+") {
+            currentValue += arr[2];
+        } else if (arr[1] === "-") {
+            currentValue -= arr[2];
+        } else if (arr[1] === "x") {
+            currentValue *= arr[2];
+        } else if (arr[1] === "/") {
+            if (arr[2] === 0) {
+                alert("Cannot divide by Zero");
+            } else {
+                currentValue /= arr[2];
+            }
+        }
+    }
+    arr = [currentValue, arr[1]];
+    return currentValue;
+}
 
 function myFunc(e) {
     const btnPressed = e.target;
     if (btnPressed.className === "number") {
-        current += e.target.innerText;
-    } else if (btnPressed.className === "dot" && dotIsPressed===false) {
+        if (operator !== "") {
+            arr[1] = operator;
+            operator = "";
+        }
+        if(current===0){
+            current="";
+        }
+        current += btnPressed.innerText;
+        document.getElementById('current').innerText = `${current}`;
+    } else if (btnPressed.className === "dot" && dotIsPressed === false) {
+        current += ".";
+        document.getElementById('current').innerText = `${current}`;
         dotIsPressed = true;
-        current += e.target.innerText;
+    } else if (btnPressed.className === "operator orange") {
+        dotIsPressed = false;
+        operator = btnPressed.innerText;
+        if (btnPressed.id === "percentage") {
+            current = (Number(current) / 100).toString();
+            document.getElementById('current').innerText = `${current}`;
+        } else {
+            arr.push(Number(current));
+            currentValue = evaluate();
+            if (btnPressed.innerText === "=") {
+                arr = [];
+                document.getElementById('result').innerText = `Ans= ${currentValue}`;
+                document.getElementById('current').innerText = "";
+            } else if (btnPressed.id === "reset") {
+                currentValue = 0;
+                arr = [];
+                current = "";
+                document.getElementById('result').textContent = `0`;
+                document.getElementById('current').textContent = "0";
+            } else {
+                document.getElementById('result').innerText = `${currentValue} ${operator}`;
+                document.getElementById('current').innerText = "";
+            }
+            current = "";
+        }
+    } else if (btnPressed.id === "backward") {
+        current = current.slice(0, -1);
+        if (current.length === 0) current = 0;
+        document.getElementById('current').innerText = `${current}`;
+    } else if (btnPressed.id === "clear") {
+        current = "";
+        document.getElementById('current').innerText = `0`;
     }
-    else if(btnPressed.className==="operator"){
-    //    TODO: when operator is pressed...
-    }
-    document.getElementById('current').textContent = `${current}`;
 }
 
 window.addEventListener('click', myFunc);
